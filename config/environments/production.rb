ef7e02705c73f897d2806fec716d01788ad8f1f7
Somwang <somwang@gmail.com>
2018-02-05 23:29:06 +0000

    Update cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.web_socket_server_url = "wss://java-mcla.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://java-mcla.herokuapp.com/', 'http://java-mcla.herokuapp.com/']
+  config.web_socket_server_url = "wss://dtctech.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://dtctech.herokuapp.com', 'http://dtctech.herokuapp.com/']
 end
