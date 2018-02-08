9001649c1402e470e83ad6022952130eb26278c5
Somwang <somwang@gmail.com>
2018-02-08 15:51:00 +0000

    Update Stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :registrations => 'registrations' }
+  # devise_for :chefs, :controllers => { :registrations => 'registrations' }
   root "pages#home"
   get 'pages/home', to: 'pages#home'
   
